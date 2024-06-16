9488e16a57eafa775581d64e178efaf21b329999
Ikkisoad <henrique.alberto.andrade@hotmail.com>
2024-06-16 11:31:31 -0300

    Resource manager

--- scenes/player_res/base_player_res/base_player_res.gd
+++ scenes/player_res/base_player_res/base_player_res.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 @onready var tmr_base_player_resource = $tmrBasePlayerResource
 
 @export var tmrDespawnMin = 1
-@export var tmrDespawnMax = 2
+@export var tmrDespawnMax = 1
 var playerResourceType = Global.PlayerResourceType.LEAVES
 
 var resourceScene
@@ -11,14 +11,16 @@ var resourceScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	startDespawnTimer()
-	setResourceScene()
+	#setResourceScene()
 
 #func _physics_process(delta):
 	#velocity.y += gravity * delta
 	#move_and_slide()
 
 func _on_tmr_base_player_resource_timeout():
-	#spawnResource()
+	match playerResourceType:
+		Global.PlayerResourceType.LEAVES:
+			get_parent().spawnTree(global_position)
 	queue_free()
 
 func startDespawnTimer():
@@ -29,15 +31,15 @@ func _on_a_2d_base_player_resource_body_entered(_body):
 	#Log.print("Body detected")
 	pass
 
-func setResourceScene():
-	match playerResourceType:
-		Global.PlayerResourceType.LEAVES:
-			resourceScene = preload("res://scenes/res/tree_res/newRes.tscn")
-
-#TODO pqq isso n ta funcionando?
-func spawnResource():
-	var newResource = resourceScene.instantiate()
-	#get_parent().add_sibling(newResource)
-	add_child(newResource)
-	newResource.global_position = global_position
+#func setResourceScene():
+	#match playerResourceType:
+		#Global.PlayerResourceType.LEAVES:
+			#resourceScene = preload("res://scenes/res/tree_res/newRes.tscn")
+#
+##TODO pqq isso n ta funcionando?
+#func spawnResource():
+	#var newResource = resourceScene.instantiate()
+	##get_parent().add_sibling(newResource)
+	#add_child(newResource)
+	#newResource.global_position = global_position
 	
---
+++ scenes/res/ResourceManager.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+@export var maxNewXPos = 350
+@export var minNewXPos = 150
+
+@export var maxTrees = 5
+var treeCount = 0
+
+@export var treeYPos = 0
+
+var treeRes = preload("res://scenes/res/tree_res/tree_res.tscn")
+
+func _ready():
+	countResources()
+
+func spawnTree(globalPos):
+	if treeCount > maxTrees: return
+	var newTree = treeRes.instantiate()
+	add_child(newTree)
+	newTree.global_position = Vector2(globalPos.x + randi_range(minNewXPos, maxNewXPos), treeYPos)
+	treeCount += 1
+
+func countResource(group):
+	var count = 0
+	for c in get_children():
+		if c.is_in_group(group):
+			count += 1
+	return count
+
+func countResources():
+	treeCount = countResource("tree")
+	
