0def94b19a248622c18bf31d8fc04243a989b21f
Ikkisoad <henrique.alberto.andrade@hotmail.com>
2024-06-21 11:29:17 -0300

    Caipora attacks

--- scenes/player/caipora/caipora.gd
+++ scenes/player/caipora/caipora.gd
@@ -1,26 +1,46 @@
 extends PlayerClass
+@onready var collision_shape_2d = $attackHitbox/CollisionShape2D
+@onready var tmr_attacking = $tmrAttacking
+@onready var tmr_dash_cooldown = $tmrDashCooldown
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	if !locked: 
-		handleInputs()
-		handleAbilities()
-	else:
-		walkStop()
+	
+	if tmr_attacking.is_stopped():
+		if !locked: 
+			handleInputs()
+			handleAbilities()
+		else:
+			walkStop()
 	move_and_slide()
 
 func handleAbilities():
-	if is_on_floor():
-		if Input.is_action_just_pressed("trap"):
-			#attack
-			spawnTrap()
-		if Input.is_action_just_pressed("setResource"):
-			#dash
-			spawnResource()
+	#if is_on_floor():
+	if Input.is_action_just_pressed("trap"):
+		#attack
+		attack()
+	if Input.is_action_just_pressed("setResource") && tmr_dash_cooldown.is_stopped():
+		#dash attack
+		dashAttack()
 	if Input.is_action_just_pressed("changeSelectedResource"):
 		get_parent().changeSelectedResource()
 	if Input.is_action_just_pressed("changeSelectedTrap"):
 		get_parent().changeSelectedTrap()
+
+func dashAttack():
+	collision_shape_2d.set_deferred("disabled", false)
+	tmr_attacking.start(0.2)
+	velocity.x = 1500 * facing
+	tmr_dash_cooldown.start(2)
+
+func attack():
+	walkStop()
+	collision_shape_2d.set_deferred("disabled", false)
+	animated_sprite_2d.play("attack")
+	tmr_attacking.start(1)
+
+
+func _on_tmr_attacking_timeout():
+	collision_shape_2d.set_deferred("disabled", true)
